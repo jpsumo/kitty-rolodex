2852b6e79486e33cee028a2dc681cc90c382367e
jpsumo <jaredmpatch@gmail.com>
2019-12-04 09:25:10 -0600

    Updates

---
+++ precache-manifest.1a9625e6a66b7c5ff0b6307857114c12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72619e28b0c91e1ce6ee39661359af0",
+    "url": "/kitty-rolodex/index.html"
+  },
+  {
+    "revision": "45acd9ce18b869f84476",
+    "url": "/kitty-rolodex/static/css/main.0b9c56cc.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/kitty-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "45acd9ce18b869f84476",
+    "url": "/kitty-rolodex/static/js/main.603880ee.chunk.js"
+  },
+  {
+    "revision": "519bbb462af5a6ff8f7b",
+    "url": "/kitty-rolodex/static/js/runtime-main.cacf25a6.js"
+  }
+]);
